be8e82ad3cb5a179dc72ad8b78df82b1663fa083
Ridho <42725295+raprap22@users.noreply.github.com>
2018-09-10 10:12:28 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -8,4 +8,17 @@ for ($i=1; $i<$var ; $i++) {
 	}
 	echo "<br>";
 }
+$n = 0;
+for ($i=0; $i < $var ; $i++) { 
+	# code...
+	$n = ($n+2);
+	echo $n;
+}
+echo "<br>";
+$n = 0;
+for ($i=0; $i < $var ; $i++) { 
+	# code...
+	$n = ($n+2);
+	echo $n-1;
+}
 ?>
